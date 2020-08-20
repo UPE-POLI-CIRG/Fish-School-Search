321c73afa45610be790ad6e49a0f3767d1eb9fc7
Hugo Amorim <hugo.amorim.neto@hotmail.com>
2020-08-20 16:50:39 -0300

    added FSS vanilla for java,python and wFSS for java

---
+++ VANILLA_FSS_rev20110129/src/fss2008vanilla/FSS_Solution.java
@@ -0,0 +1,36 @@
+package fss2008vanilla;
+
+import cec2010suite.Function;
+
+
+/*
+ * Copyright (c) 2011 Murilo Rebelo Pontes
+ * murilo.pontes@gmail.com
+ * 
+ * GNU LESSER GENERAL PUBLIC LICENSE (Version 2.1, February 1999)
+ */
+
+
+
+public class FSS_Solution {
+	//
+	public double[] variables = null;
+	public double fitness = Double.NaN;
+	//
+	public FSS_Solution(int dims) {
+		variables = new double[dims];
+	}
+	public static void copy(FSS_Solution source,FSS_Solution destination){
+		//
+		destination.fitness = source.fitness;
+		//
+		for(int i=0;i<source.variables.length;i++){
+			destination.variables[i]=source.variables[i];
+		}
+	}
+	public static void randomize_variables(FSS_Solution sol,FSS_PRNG r,Function f){
+		for(int i=0;i<sol.variables.length;i++){
+			sol.variables[i]=r.between(f.getMin(), f.getMax());
+		}
+	}
+}
